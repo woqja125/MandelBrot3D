3a852c54916bd10fccd47442532fd626015659e9
jb <woqja125@gmail.com>
2014-06-03 09:47:48 +0900

    Add JB_3DMandelView

---
+++ MandelBrot3D/JB_3DMandelView.h
@@ -0,0 +1,13 @@
+//
+//  JB_3DMandelView.h
+//  MandelBrot3D
+//
+//  Created by 이재범 on 2014. 6. 3..
+//  Copyright (c) 2014년 jb. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface JB_3DMandelView : NSOpenGLView
+
+@end
---
+++ MandelBrot3D/JB_3DMandelView.m
@@ -0,0 +1,29 @@
+//
+//  JB_3DMandelView.m
+//  MandelBrot3D
+//
+//  Created by 이재범 on 2014. 6. 3..
+//  Copyright (c) 2014년 jb. All rights reserved.
+//
+
+#import "JB_3DMandelView.h"
+
+@implementation JB_3DMandelView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
