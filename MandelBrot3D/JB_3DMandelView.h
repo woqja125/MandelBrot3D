28789bfc3fa9913c8d0f0cf55d1206e82a3e6287
jb <woqja125@gmail.com>
2014-06-18 18:21:37 +0900

    prepare use vertex array in 3dView

--- MandelBrot3D/JB_3DMandelView.h
+++ MandelBrot3D/JB_3DMandelView.h
@@ -28,6 +28,12 @@
 	Matrix4fT    Transform;
 	Matrix3fT    LastRot;
 	Matrix3fT    ThisRot;
+	
+	float ver[(800*600+10000)*3];
+	float col[(800*600+10000)*3];
+	float nor[(800*600+10000)*3];
+	
+	unsigned Index[(800*600+10000)*6], indexCnt;
 
 }
 
